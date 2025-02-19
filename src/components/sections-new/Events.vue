6f70aca55ae2c0e45ac09639dbf7df26fddacc3b
Mateusz Borkowski <mateusz@bizwebstudio.pl>
2025-02-19 13:48:21 +0100

    Zmiany praca 19.02

---
+++ src/components/sections-new/Events.vue
@@ -0,0 +1,135 @@
+<script>
+</script>
+
+<template>
+    <div class="event col-12">
+        <div class="event-img">
+            <a class="link-stretched" href="/">
+            </a>
+            <img src="/src/assets/images/events-bg-1.jpg" alt="">
+        </div>
+
+        <div class="d-flex flex-column mr-auto">
+            <h6 class="event-title"><a class="" href="">Bang Pow Boom at Juggaloo island</a></h6>
+            <p class="event-date">December 20, 2022</p>
+        </div>
+        <a class="read-more">
+            Manic Depressive
+            <i class="fa fa-arrow-right"></i>
+        </a>
+    </div>
+    <div class="event col-12">
+        <div class="event-img">
+            <a class="link-stretched" href="/">
+            </a>
+            <img src="/src/assets/images/events-bg-1.jpg" alt="">
+        </div>
+
+        <div class="d-flex flex-column mr-auto">
+            <h6 class="event-title"><a class="" href="">Bang Pow Boom at Juggaloo island</a></h6>
+            <p class="event-date">December 20, 2022</p>
+        </div>
+        <a class="read-more">
+            Manic Depressive
+            <i class="fa fa-arrow-right"></i>
+        </a>
+    </div>
+    <div class="event col-12">
+        <div class="event-img">
+            <a class="link-stretched" href="/">
+            </a>
+            <img src="/src/assets/images/events-bg-1.jpg" alt="">
+        </div>
+
+        <div class="d-flex flex-column mr-auto">
+            <h6 class="event-title"><a class="" href="">Bang Pow Boom at Juggaloo island</a></h6>
+            <p class="event-date">December 20, 2022</p>
+        </div>
+        <a class="read-more">
+            Manic Depressive
+            <i class="fa fa-arrow-right"></i>
+        </a>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+.event {
+    display: flex;
+    flex-direction: column;
+    padding-top: 25px;
+    margin-bottom: 25px;
+    position: relative;
+
+    @include media-breakpoint-up(md) {
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    &::before {
+        height: 1px;
+        display: block;
+        width: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        position: absolute;
+        content: '';
+        background-color: var(--dark);
+    }
+
+    &-img {
+        max-width: 170px;
+        flex-shrink: 0;
+        margin-bottom: 20px;
+        position: relative;
+
+        @include media-breakpoint-up(lg) {
+            margin-right: 30px;
+            margin-bottom: 0;
+        }
+    }
+
+    &-title {
+        margin-bottom: 5px;
+        font-size: 28px;
+        line-height: 36px;
+        font-weight: 500;
+
+        a {
+            text-decoration: underline;
+            text-decoration-color: transparent;
+            text-decoration-thickness: 0.08em;
+            text-underline-offset: 0.05em;
+            transition: all 0.45s;
+        }
+
+
+        @include media-breakpoint-up(lg) {
+            margin-bottom: 0;
+        }
+
+        &:hover {
+            a {
+                text-decoration-color: var(--dark);
+            }
+
+        }
+    }
+
+    &-date {
+        font-family: var(--additonal);
+        font-weight: 400;
+        color: #505558;
+        font-size: 12px;
+        line-height: 30px;
+        text-transform: uppercase;
+        margin-bottom: 10px;
+
+        @include media-breakpoint-up(md) {
+            margin-bottom: 0;
+        }
+    }
+}
+</style>
--- src/views/New.vue
+++ src/views/New.vue
@@ -1,13 +1,80 @@
 <script setup>
-  import UpcomingEvents from '@/components/sections/UpcomingEvents.vue';
-  import MainFooter from '@/components/sections-new/MainFooter.vue';
-  import Team from '@/components/sections-new/Team.vue';
+import UpcomingEvents from '@/components/sections/UpcomingEvents.vue';
+import MainFooter from '@/components/sections-new/MainFooter.vue';
+import Team from '@/components/sections-new/Team.vue';
+import Services from '@/components/sections-new/Services.vue'
+import CategoriesImg from '@/components/sections-new/Categories-img.vue';
+import Events from '@/components/sections-new/Events.vue'
+
+
+
+const team = [
+  {
+    id: 1,
+    teamImg: '/src/assets/images/team-1.jpg',
+    teamLink: '/chad',
+    teamLinkFb: '/fb',
+    teamLinkTw: '/tw',
+    teamLinkIns: '/ins',
+    teamName: 'Chad Gigacock',
+    teamProps: 'HNIC',
+  },
+  {
+    id: 2,
+    teamImg: '/src/assets/images/team-1.jpg',
+    teamLink: '/chad',
+    teamLinkFb: '/fb',
+    teamLinkTw: '/tw',
+    teamLinkIns: '/ins',
+    teamName: 'Chad Gigacock',
+    teamProps: 'HNIC',
+  },
+  {
+    id: 3,
+    teamImg: '/src/assets/images/team-1.jpg',
+    teamLink: '/chad',
+    teamLinkFb: '/fb',
+    teamLinkTw: '/tw',
+    teamLinkIns: '/ins',
+    teamName: 'Chad Gigacock',
+    teamProps: 'HNIC',
+  },
+  {
+    id: 4,
+    teamImg: '/src/assets/images/team-1.jpg',
+    teamLink: '/chad',
+    teamLinkFb: '/fb',
+    teamLinkTw: '/tw',
+    teamLinkIns: '/ins',
+    teamName: 'Chad Gigacock',
+    teamProps: 'HNIC',
+  },
+  {
+    id: 5,
+    teamImg: '/src/assets/images/team-1.jpg',
+    teamLink: '/chad',
+    teamLinkFb: '/fb',
+    teamLinkTw: '/tw',
+    teamLinkIns: '/ins',
+    teamName: 'Chad Gigacock',
+    teamProps: 'HNIC',
+  },
+];
 </script>
 
-
 <template>
-
-<Team 
-
-/>
+  <h1>ZMIEŃ WSZYSTKIE KLAS- na KLAS__ you mongrel</h1>
+  <CategoriesImg class="pt-70px pt-lg-120px pb-60px pb-lg-90px" />
+  <section>
+    <div class="container flex-column align-items-center">
+      <p class="sub-title sub-title-lprpl mb-20px">FUTURE EVENTS</p>
+      <h3 class="title-1 mb-20px">Festivals This December </h3>
+      <Events />
+    </div>
+  </section>
+  <Services />
+  <Team :team="team">
+    <template #subtitle>Meet Our Team</template>
+    <template #title>Our Professionals</template>
+  </Team>
 </template>
